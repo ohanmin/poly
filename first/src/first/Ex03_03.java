7bb3734263ef25826a5e9f00100846acba3d1d17
data25 <data25@DESKTOP-Q60QRVH>
2021-03-22 16:12:48 +0900

    ex03_03

---
+++ first/src/first/Ex03_03.java
@@ -0,0 +1,11 @@
+package first;
+
+public class Ex03_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(100+"/"+ 200+"="+0.5);
+		System.out.println("\n");
+	}
+
+}
