491531ab084e6257da27e04dabe138685744cdfd
data25 <data25@DESKTOP-Q60QRVH>
2021-03-24 12:33:53 +0900

    자바 끝

---
+++ first/src/first/Ex04_03.java
@@ -0,0 +1,28 @@
+package first;
+
+public class Ex04_03 {
+public static void main(String[] args) {
+	int a = 10;
+	a++;
+	System.out.println(a);
+	a--;
+	System.out.println(a);
+	a += 5;
+	System.out.println(a);
+	a -= 5;
+	System.out.println(a);
+	a *= 5;
+	System.out.println(a);
+	a /=5;
+	System.out.println(a);
+	a %=5;
+	System.out.println(a);
+	a= 10;
+	a ++;
+	System.out.println("a : " + a);
+	System.out.println("a++ : " + a++);
+	System.out.println("a : " + a);
+	System.out.println("++a : "+ ++a);
+	
+}
+}
---
+++ first/src/first/Ex05_01.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Ex05_01 {
+	public static void main(String[] args) {
+		int a = 2000;
+		if (a < 100) {
+			System.out.println("오.. 100보다 작군요~");
+			System.out.println("거짓으므로 이문장은 보이지 않겠네요");
+		} else {
+			System.out.println("100보다 크네");
+		}
+
+	}
+}
